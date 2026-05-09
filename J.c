092a734f8d8dc584b22458293a329fd7b22b2bba
bijali-jlakshmi <bijalijayalakshmijayan@gmail.com>
2026-05-09 17:02:07 +0530

    adding

---
+++ J.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int i, j, n=7;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++)
+        {
+            if(i==0 || j==n-1 || (i==n-1 && j==0) ||(i==n-1)||(i==n-2 && j==0))
+            printf("*");
+            else
+            printf(" ");
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ k.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int i, j, size, m=0, n=5;
+    printf("Enter size");
+    scanf("%d", &size);
+    for(i=0;i<=size;i++)
+    {
+        for(j=0;j<=size-1;j++)
+        {
+            if(j==1 || (i==j+1 && j!=0))
+            {
+                printf("*");
+            }
+            else if(i==m && j==n)
+            {
+                printf("* ");
+                m=m+1;
+                n=n+1;
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
